4f9178d734effe0f80392dc0d6904888f7f9e660
Balasubramanian Palaniappan <bpalaniappan@jobcase.com>
2023-07-26 00:42:26 -0500

    updated app with new example

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/images/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from 'react';
+// main app component.
+const App = () => {
+  const [inputValue, setInputValue] = useState('Howdy');
 
-function App() {
-  const [count, setCount] = useState(0);
+  const handleOnChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   return (
     <>
-      <div className="md:container md:mx-auto p-4">
-        <div>
-          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div>
-          <button
-            className="btn btn-primary"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is {count}
-          </button>
-        </div>
+      <div>
+        <h2>Hello World!</h2>
+        <input
+          type="text"
+          name="data"
+          id="data"
+          placeholder="Enter something"
+          onChange={handleOnChange}
+        />
+        <br />
+        {inputValue}
       </div>
     </>
   );
-}
+};
 
 export default App;

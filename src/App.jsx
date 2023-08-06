fb8c4a2aa71403894804573bc3362106574df239
Balasubramanian Palaniappan <bpalaniappan@jobcase.com>
2023-08-06 18:56:49 -0500

    templates and header component added with daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import UserList from './components/UserList';
 import Sample from './components/Sample';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 // main app component.
 const App = () => {
   return (
     <>
-      <div className="container mx-auto font-sans">
+      <div className="md:container md:mx-auto px-4 py-4 font-sans">
+        <Header />
         <Sample />
         <div className="divider"></div>
         <UserList />
